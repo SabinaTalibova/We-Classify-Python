96170f4ffbaf823bb3b822be027ac97db30cc477
Sabina Talibova <sabinatalibova1@gmail.com>
2018-02-14 03:22:18 +0400

    cannot save non english chars

--- scraper.py
+++ scraper.py
@@ -1,23 +1,45 @@
 import bs4 as bs
 import urllib.request
+import  csv
 
 source = urllib.request.urlopen('https://oxu.az/politics').read()
 
 
-soup = bs.BeautifulSoup(source,'lxml')
+
+#soup = bs.BeautifulSoup(source,'lxml')
+soup = bs.BeautifulSoup(source.decode('utf-8','ignore'), 'lxml')
+
 data=soup.prettify()
-#print(data)
+
+#a way to output formatted source code to file for easy analysing
 #with open("Output.html", "w",encoding="utf8") as text_file:
     #text_file.write(data)
 
 
-for link in soup.find_all('a',{"class":"news-i-inner"}):
-    l=link.get('href')
-    source1=urllib.request.urlopen('https://oxu.az'+l).read()
-    soup=bs.BeautifulSoup(source1,'lxml')
-    data1=soup.prettify()
-    with open("Output1.html", "w",encoding="utf8") as text_file:
-    	text_file.write(data)
+
+with open('onehot.csv','w',encoding="utf8") as testfile:
+    csv_writer=csv.writer(testfile)
+
+
+
+##now find links in new-i-inner class
+    for link in soup.find_all('a', {"class":"news-i-inner"}):
+        l=link.get('href')
+    #print(l)
+
+        source = urllib.request.urlopen('https://oxu.az'+l).read()
+        soup = bs.BeautifulSoup(source,'lxml')
+        data=soup.prettify()
+        news=soup.find('div', {"class":"news-inner"}).text.encode('UTF-8')
+        csv_writer.writerow([news])
+
+
+  
+
+
+
+
+
 
 
 
