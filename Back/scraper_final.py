0264dfa031424b315492d69a44e2cc544fe019e7
SabinaTalibova <sabinatalibova1@gmail.com>
2018-02-23 06:20:01 -0800

    Add files via upload

--- Back/scraper_final.py
+++ Back/scraper_final.py
@@ -2,14 +2,13 @@ import bs4 as bs
 import urllib.request
 import  csv
 import time
-from multiprocessing import Pool
 
 
 
 
 global link
 
-link='https://oxu.az/world'
+link='https://oxu.az/turism'
 def find_news_links(link):
     links=[]
     req = urllib.request.Request(
@@ -18,10 +17,11 @@ def find_news_links(link):
     headers={
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36'
         ,'From': 'sabinatalibova1@gmail.com'
-    }
+
+    },
 
     )
-    source = urllib.request.urlopen(req).read()
+    source = urllib.request.urlopen(req,timeout=120).read()
     soup = bs.BeautifulSoup(source, 'lxml')
     #data=soup.prettify()
     next_page=soup.find('a',{"class":"more"})
@@ -31,18 +31,19 @@ def find_news_links(link):
         links.append(l)
     return links,pagination_link
 def scrape_news_content():
-    with open('data.csv','a',encoding="utf8") as file:
+    with open('turism.csv','a',encoding="utf8") as file:
         writer=csv.writer(file)
         links=find_news_links(link)[0]
         for i in links:
             req = urllib.request.Request(
-                link,data=None, 
+                'https://oxu.az'+i,data=None, 
                 headers={
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36'
         ,'From': 'sabinatalibova1@gmail.com'
-    }
-)            
-            source = urllib.request.urlopen(req).read()
+    },
+
+)           
+            source = urllib.request.urlopen(req,timeout=120).read()
             #source=urllib.request.urlopen('https://oxu.az'+i).read()
             soup=bs.BeautifulSoup(source,'lxml')
             #data=soup.prettify()
@@ -50,15 +51,14 @@ def scrape_news_content():
             #label='siyast'
             #writer.writerow(zip([news_text],[label]))
             writer.writerow([news_text])
-            time.sleep(2)
+            time.sleep(3)
 global i
 i=0
-while i<2:
-    try:
-        link='https://oxu.az'+find_news_links(link)[1]
-        scrape_news_content()
-        i=i+1
-        time.sleep(3)
-    except:
-        coverage=" stopped scraping "
+while i<200:
+    link='https://oxu.az'+find_news_links(link)[1]
+    scrape_news_content()
+    i=i+1
+    time.sleep(3)
+    print(i)
+  
 
