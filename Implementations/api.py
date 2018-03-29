2125b5b217c394ccb9096c64d836282b48b1bfcf
unknown <stalibova2018@ada.edu.az>
2018-03-29 07:34:48 -0700

    web simple

--- Implementations/api.py
+++ Implementations/api.py
@@ -1,25 +1,34 @@
-
 import flask
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from sklearn.externals import joblib
+import numpy as np
+from scipy import misc
 
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
-@app.route("/predict",methods=['POST'])
-
-
 def index():
-   return flask.render_template('index.html')
+    return flask.render_template('index.html')
 
 
-def rediction():
+@app.route('/predict', methods=['POST'])
+def make_prediction():
 	if request.method=='POST':
-		return render_template('index.html',label="3")
+
+		# get uploaded image file if it exists
+		text=[]
+		txt = request.form['text']
+		text.append(txt)
+
+		
+		prediction = svm.predict(text)
+		label=prediction
+	
+		return render_template('index.html', label=label)
 
 
 if __name__ == '__main__':
-	model=joblib.load('svm.pkl')
+	svm = joblib.load('svm.pkl')
 	app.run(host='0.0.0.0', port=8000, debug=True)
